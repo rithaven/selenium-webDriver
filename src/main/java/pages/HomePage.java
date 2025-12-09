1b90df4a1cdee3d46724950bb8847e38b46066d5
rithaven <rita.git@gmail.com>
2025-12-09 12:12:43 +0200

    feat: interacting with drodown elements

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -5,16 +5,20 @@ import org.openqa.selenium.WebDriver;
 
 public class HomePage {
     private WebDriver driver;
-    private By formAuthenticationLink = By.linkText("Form Authentication");
 
     public  HomePage(WebDriver driver){
         this.driver = driver;
     }
     public LoginPage clickAuthenticationLink(){
-        driver.findElement(formAuthenticationLink).click();
-        //we should return handle to that page
+       clickLink("Form Authentication");
         return new LoginPage(driver);
     }
+    public DropdownPage clickDropDown(){
+        clickLink("Dropdown");
+        return new DropdownPage(driver);
+    }
 
-
+    private void clickLink(String linkText){
+        driver.findElement(By.linkText(linkText)).click();
+}
 }
